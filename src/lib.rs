1f9d20cc55a48ad871934f1fd95dbdc58f073df3
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2026-06-23 13:07:17 +0200

    Add `as_*` conversions.

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,9 @@ pub struct Pow2 {
     exponent: u8,
 }
 
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+pub struct NotPow2;
+
 impl Pow2 {
     pub fn from_exponent(exponent: u8) -> Pow2 {
         Pow2 { exponent }
@@ -17,8 +20,29 @@ impl Pow2 {
     }
 }
 
-#[derive(Debug, Eq, PartialEq, Copy, Clone)]
-pub struct NotPow2;
+macro_rules! impl_as {
+    ($name:ident, $t:ty) => {
+        impl Pow2 {
+            pub fn $name(self) -> $t {
+                1 << self.exponent
+            }
+        }
+    };
+}
+
+impl_as!(as_u8, u8);
+impl_as!(as_u16, u16);
+impl_as!(as_u32, u32);
+impl_as!(as_u64, u64);
+impl_as!(as_u128, u128);
+impl_as!(as_usize, usize);
+
+impl_as!(as_i8, i8);
+impl_as!(as_i16, i16);
+impl_as!(as_i32, i32);
+impl_as!(as_i64, i64);
+impl_as!(as_i128, i128);
+impl_as!(as_isize, isize);
 
 macro_rules! impl_try_from {
     ($t:ty) => {
@@ -73,6 +97,8 @@ mod tests {
         assert_eq!(Pow2::try_from(32_u64), Ok(Pow2::from_exponent(5)));
         assert_eq!(Pow2::try_from(32_i128), Ok(Pow2::from_exponent(5)));
         assert_eq!(Pow2::try_from(32_u128), Ok(Pow2::from_exponent(5)));
+        assert_eq!(Pow2::try_from(32_isize), Ok(Pow2::from_exponent(5)));
+        assert_eq!(Pow2::try_from(32_usize), Ok(Pow2::from_exponent(5)));
     }
 
     #[test]
@@ -99,5 +125,36 @@ mod tests {
     #[test]
     fn try_from_max() {
         assert_eq!(Pow2::try_from(i64::MAX), Err(NotPow2));
+    }
+
+    #[test]
+    fn as_int_all_types() {
+        let p = Pow2::from_exponent(6);
+        assert_eq!(p.as_i8(), 64);
+        assert_eq!(p.as_u8(), 64);
+        assert_eq!(p.as_i16(), 64);
+        assert_eq!(p.as_u16(), 64);
+        assert_eq!(p.as_i32(), 64);
+        assert_eq!(p.as_u32(), 64);
+        assert_eq!(p.as_i64(), 64);
+        assert_eq!(p.as_u64(), 64);
+        assert_eq!(p.as_i128(), 64);
+        assert_eq!(p.as_u128(), 64);
+        assert_eq!(p.as_isize(), 64);
+        assert_eq!(p.as_usize(), 64);
+    }
+
+    #[test]
+    fn as_int_max_fitting() {
+        assert_eq!(Pow2::from_exponent(6).as_i8(), 1 << 6);
+        assert_eq!(Pow2::from_exponent(7).as_u8(), 1 << 7);
+        assert_eq!(Pow2::from_exponent(14).as_i16(), 1 << 14);
+        assert_eq!(Pow2::from_exponent(15).as_u16(), 1 << 15);
+        assert_eq!(Pow2::from_exponent(30).as_i32(), 1 << 30);
+        assert_eq!(Pow2::from_exponent(31).as_u32(), 1 << 31);
+        assert_eq!(Pow2::from_exponent(62).as_i64(), 1 << 62);
+        assert_eq!(Pow2::from_exponent(63).as_u64(), 1 << 63);
+        assert_eq!(Pow2::from_exponent(126).as_i128(), 1 << 126);
+        assert_eq!(Pow2::from_exponent(127).as_u128(), 1 << 127);
     }
 }
