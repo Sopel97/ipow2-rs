08f9bd96a3b037208e761b89f29caa7afc6bcc41
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2026-06-27 15:09:55 +0200

    Add some internal benchmarks.

--- benches/internal.rs
+++ benches/internal.rs
@@ -26,6 +26,23 @@ fn make_inputs_i64() -> [(i64, u8); 256] {
     inputs
 }
 
+fn make_inputs_i64_2() -> [(i64, i64, u8); 256] {
+    let inputs: [(i64, i64, u8); 256] = std::array::from_fn(|i| {
+        (
+            123123123123123i64 + i as i64,
+            2151323151123i64 + i as i64,
+            1u8 + (i % 13) as u8,
+        )
+    });
+    inputs
+}
+
+fn make_inputs_i64_mask() -> [(i64, i64); 256] {
+    let inputs: [(i64, i64); 256] =
+        std::array::from_fn(|i| (123123123123123i64 + i as i64, 1 << (1u8 + (i % 13) as u8)));
+    inputs
+}
+
 mod div_floor {
     use super::*;
 
@@ -115,7 +132,9 @@ mod ceil_to_multiple {
 }
 
 mod div_ceil {
-    use crate::{make_inputs_i16, make_inputs_i32, make_inputs_i64};
+    use crate::{
+        make_inputs_i16, make_inputs_i32, make_inputs_i64, make_inputs_i64_2, make_inputs_i64_mask,
+    };
 
     #[divan::bench]
     fn div_ceil_mask_a_i32_via_i64(bencher: divan::Bencher) {
@@ -199,6 +218,120 @@ mod div_ceil {
                     let floored = a >> b;
                     let rem = a - (floored << b);
                     divan::black_box(floored + (rem != 0) as i64);
+                }
+            });
+    }
+
+    #[divan::bench]
+    fn div_ceil_no_mask_i64_2(bencher: divan::Bencher) {
+        bencher
+            .with_inputs(|| make_inputs_i64_2())
+            .bench_values(|inputs| {
+                for (a, c, b) in inputs {
+                    let flooreda = a >> b;
+                    let flooredc = c >> b;
+                    let rema = a - (flooreda << b);
+                    let remc = c - (flooredc << b);
+                    divan::black_box((
+                        flooreda + (rema != 0) as i64,
+                        flooredc + (remc != 0) as i64,
+                    ));
+                }
+            });
+    }
+
+    #[divan::bench]
+    fn div_ceil_no_mask_i64_kindamask(bencher: divan::Bencher) {
+        bencher
+            .with_inputs(|| make_inputs_i64())
+            .bench_values(|inputs| {
+                for (a, b) in inputs {
+                    let mask = (1 << b) - 1;
+                    let floored = a >> b;
+                    divan::black_box(floored + (a & mask != 0) as i64);
+                }
+            });
+    }
+
+    #[divan::bench]
+    fn div_ceil_no_mask_i64_kindamask_2(bencher: divan::Bencher) {
+        bencher
+            .with_inputs(|| make_inputs_i64_2())
+            .bench_values(|inputs| {
+                for (a, c, b) in inputs {
+                    let mask = (1 << b) - 1;
+                    let flooreda = a >> b;
+                    let flooredc = c >> b;
+                    divan::black_box((
+                        flooreda + (a & mask != 0) as i64,
+                        flooredc + (c & mask != 0) as i64,
+                    ));
+                }
+            });
+    }
+
+    #[divan::bench]
+    fn div_ceil_from_mask_i64(bencher: divan::Bencher) {
+        bencher
+            .with_inputs(|| make_inputs_i64_mask())
+            .bench_values(|inputs| {
+                for (a, mask) in inputs {
+                    let b = mask.trailing_ones();
+                    let floored = a >> b;
+                    divan::black_box(floored + (a & mask != 0) as i64);
+                }
+            });
+    }
+}
+
+mod div_round {
+    use crate::make_inputs_i64;
+
+    // NOTE: We CANNOT do b-1, so all of these, while nice, don't cut it.
+    #[divan::bench]
+    fn div_round_a(bencher: divan::Bencher) {
+        bencher
+            .with_inputs(|| make_inputs_i64())
+            .bench_values(|inputs| {
+                for (a, b) in inputs {
+                    let floored = a >> b;
+                    divan::black_box(floored + ((a & (1 << (b - 1))) != 0) as i64);
+                }
+            });
+    }
+
+    #[divan::bench]
+    fn div_round_b(bencher: divan::Bencher) {
+        bencher
+            .with_inputs(|| make_inputs_i64())
+            .bench_values(|inputs| {
+                for (a, b) in inputs {
+                    let mask = (1 << (b - 1)) - 1;
+                    divan::black_box((a + mask) >> b);
+                }
+            });
+    }
+
+    #[divan::bench]
+    fn div_round_c(bencher: divan::Bencher) {
+        bencher
+            .with_inputs(|| make_inputs_i64())
+            .bench_values(|inputs| {
+                for (a, b) in inputs {
+                    let floored = a >> b;
+                    divan::black_box(floored + (a >> (b - 1)) & 1);
+                }
+            });
+    }
+
+    #[divan::bench]
+    fn div_round_d(bencher: divan::Bencher) {
+        bencher
+            .with_inputs(|| make_inputs_i64())
+            .bench_values(|inputs| {
+                for (a, b) in inputs {
+                    let floored = a >> (b - 1);
+                    divan::black_box((floored + 1) >> 1);
                 }
             });
     }
